51f1925cc1a0a882483ff25daa59068b6004a1f6
nour-abolila <nouraliabolilanaaltya101@gmail.com>
2026-05-17 16:49:09 +0300

    Refactor ProductController to use ApiResponse for standardized JSON responses

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,50 +2,30 @@
 
 namespace App\Http\Controllers;
 
+use App\Helpers\ApiResponse;
+use App\Http\Resources\ProductResource;
 use App\Models\Product;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    
+
     public function index()
     {
         $products = Product::paginate(10);
-        return response()->json($products);
+        return ApiResponse::success(
+            'products retrieved successfully',
+            ['products' => ProductResource::collection($products)]
+        );
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    // public function store(Request $request)
-    // {
-    //     $product = Product::create($request->all());
-    //     return response()->json($product, 201);
-    // }
 
-    /**
-     * Display the specified resource.
-     */
+
     public function show(Product $product)
     {
-        return response()->json($product);
+        return ApiResponse::success(
+            'product show successfully',
+            ['product' => new ProductResource($product)]
+        );
     }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    // public function update(Request $request, Product $product)
-    // {
-    //     $product->update($request->all());
-    //     return response()->json($product);
-    // }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    // public function destroy(Product $product)
-    // {
-    //     $product->delete();
-    //     return response()->json(['message' => 'Product deleted successfully']);
-    // }
 }
