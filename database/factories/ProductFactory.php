41634d437b0bf5095241d484e336ba129078138e
nour-abolila <nouraliabolilanaaltya101@gmail.com>
2026-05-17 16:49:59 +0300

    Add UserUpdateRequest, UserResource, and ProductResource classes; update Order model and factories

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -20,9 +20,9 @@ class ProductFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => $this->faker->word(),
-            'description' => $this->faker->sentence(),
-            'price' => $this->faker->randomFloat(2, 1, 100),
+            'name' => fake()->word(),
+            'description' => fake()->sentence(),
+            'price' => fake()->randomFloat(2, 1, 100),
         ];
     }
 }
