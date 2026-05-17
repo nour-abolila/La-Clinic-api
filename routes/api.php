bdd9c773bf87d3018cb912462745cf8a2534c492
nour-abolila <nouraliabolilanaaltya101@gmail.com>
2026-05-17 16:48:55 +0300

    Add orders API resource route

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\OrderController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
@@ -13,3 +14,4 @@ Route::get('/user', function (Request $request) {
 
 Route::apiResource('users', UserController::class);
 Route::apiResource('products', ProductController::class);
+Route::apiResource('orders', OrderController::class);
