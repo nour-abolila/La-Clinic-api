9500730975bf42df17e3578f960f88e5f3d81195
nour-abolila <nouraliabolilanaaltya101@gmail.com>
2026-05-19 22:01:24 +0300

    Add pagination property and constructor to base Controller class

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -4,5 +4,10 @@ namespace App\Http\Controllers;
 
 abstract class Controller
 {
-    //
+    // This property will be used to set the default pagination value for all controllers that extend this base controller
+    protected $paginate;
+    public function __construct()
+    {
+        $this->paginate = request()->paginate ?? 10;
+    }
 }
