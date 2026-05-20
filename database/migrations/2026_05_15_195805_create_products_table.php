3df49055ae3b0752995573a50e47546180b7f87f
nour-abolila <nouraliabolilanaaltya101@gmail.com>
2026-05-20 23:03:42 +0300

    Change description column type to longText in store_products migration

--- database/migrations/2026_05_15_195805_create_products_table.php
+++ database/migrations/2026_05_15_195805_create_products_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('store_products', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->text('description');
+            $table->longText('description');
             $table->decimal('price', 8, 2);
             $table->timestamps();
         });
