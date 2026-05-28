b85a45a280c14878f4d40396288e35e2a57dee4a
nour-abolila <nouraliabolilanaaltya101@gmail.com>
2026-05-29 02:50:51 +0300

    Refactor ApiResponse class to improve formatting and consistency in JSON response structure

--- app/Helpers/ApiResponse.php
+++ app/Helpers/ApiResponse.php
@@ -4,22 +4,24 @@ namespace App\Helpers;
 
 use Illuminate\Http\JsonResponse;
 
-
 class ApiResponse
 {
     public static function success(string $message = 'Success', $data = []): JsonResponse
     {
         return response()->json([
+            'success' => true,
             'message' => $message,
-            'data'    => $data,
+            'data' => $data,
         ]);
     }
+
 
     public static function error(string $message = 'Error', $errors = []): JsonResponse
     {
         return response()->json([
+            'success' => false,
             'message' => $message,
-            'errors'  => $errors,
+            'errors' => $errors,
         ]);
     }
 }
