d6b92317a2e3c23ac7d5e56d517c33a79f58ed71
nour-abolila <nouraliabolilanaaltya101@gmail.com>
2026-05-29 02:47:23 +0300

    Add OtpMail class for sending OTP verification emails

---
+++ app/Mail/OtpMail.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Mail;
+
+use App\Models\User;
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+class OtpMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public User $user;
+    public string $otp;
+
+    public function __construct(User $user, string $otp)
+    {
+        $this->user = $user;
+        $this->otp = $otp;
+    }
+
+    public function envelope(): Envelope
+    {
+        return new Envelope(
+            subject: 'Hello , Your OTP Code for Verification',
+        );
+    }
+
+
+    public function content(): Content
+    {
+        return new Content(
+            view: 'emails.otp',    // ده الملف اللي فيه الـ HTML
+            with: ['otp' => $this->otp], // مررنا الكود للـ view 
+        );
+    }
+
+
+    /**
+     * Get the attachments for the message.
+     *
+     * @return array<int, \Illuminate\Mail\Mailables\Attachment>
+     */
+    public function attachments(): array
+    {
+        return [];
+    }
+}
