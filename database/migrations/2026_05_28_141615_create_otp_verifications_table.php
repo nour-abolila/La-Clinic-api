fa4c3321e0b3c2dd8db401ab3b2a51fb2373a3b8
nour-abolila <nouraliabolilanaaltya101@gmail.com>
2026-05-29 02:48:33 +0300

    Create migration for otp_verifications table to store OTP data and verification attempts

---
+++ database/migrations/2026_05_28_141615_create_otp_verifications_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('otp_verifications', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->string('otp_code');
+            $table->timestamp('expires_at');
+            $table->integer('attempts')->default(0); // Track OTP verification attempts helps me to use rate limiting
+            $table->timestamp('verified_at')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('otp_verifications');
+    }
+};
