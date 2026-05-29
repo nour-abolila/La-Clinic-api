63027eb52d183bc23d6382665fa472ac218c1954
nour-abolila <nouraliabolilanaaltya101@gmail.com>
2026-05-29 22:35:27 +0300

    make adminseeder

---
+++ database/seeders/AdminSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::updateOrCreate(
+            ['email' => 'admin@gmail.com'],
+            [
+                'first_name' => 'Admin',
+                'last_name' => 'System',
+                'password' => Hash::make('12345678'),
+                'email_verified_at' => now(),
+                'role' => 'admin',
+            ]
+        );
+    }
+}
