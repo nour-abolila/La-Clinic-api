5be3422bd52f01018db01181796c373d48c8cd36
nour-abolila <nouraliabolilanaaltya101@gmail.com>
2026-05-29 22:58:07 +0300

    fix some bugs

---
+++ app/Http/Requests/Category/StoreCategoryRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests\Category;
+
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreCategoryRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return $this->user() &&
+            $this->user()->role === 'admin';
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => 'required|string|max:255',
+            'slug' => 'required|string|unique:categories,slug',
+            'description' => 'nullable|string',
+        ];
+    }
+}
---
+++ app/Http/Requests/Category/UpdateCategoryRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests\Category;
+
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateCategoryRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return $this->user() &&
+            $this->user()->role === 'admin';
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => 'sometimes|string|max:255',
+            // 'slug' => 'sometimes|string|unique:categories,slug,' . $this->category,
+            'description' => 'sometimes|nullable|string',
+        ];
+    }
+}
