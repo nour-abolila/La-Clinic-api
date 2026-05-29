124a3788378b466f57291bfdbc43db98f6968f5b
nour-abolila <nouraliabolilanaaltya101@gmail.com>
2026-05-30 00:32:46 +0300

    handle api routes

--- routes/api.php
+++ routes/api.php
@@ -1,17 +1,42 @@
 <?php
 
+
+use App\Http\Controllers\Auth\AuthController;
+use App\Http\Controllers\Auth\PasswordController;
+use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\OrderController;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\ReviewController;
 use App\Http\Controllers\UserController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 
+Route::prefix('auth')->name('auth.')->group(function () {
+    Route::post('/register', [AuthController::class, 'register']);
+    Route::post('/verify-otp', [AuthController::class, 'verifyOtp']);
+    Route::post('/login', [AuthController::class, 'login']);
+    Route::post('/forget-password', [PasswordController::class, 'forgetPassword']);
+    Route::post('/verify-password', [PasswordController::class, 'verifyPassword']);
+    Route::post('/reset-password', [PasswordController::class, 'resetPassword']);
+});
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
 
-Route::apiResource('users', UserController::class);
+Route::middleware('auth:sanctum')->group(function () {
+    Route::prefix('auth')->name('auth.')->group(function () {
+        Route::post('/logout', [AuthController::class, 'logout']);
+    });
+});
+
+Route::apiResource('categories', CategoryController::class);
 Route::apiResource('products', ProductController::class);
-Route::apiResource('orders', OrderController::class);
+ 
+
+
+Route::middleware(['auth:sanctum', 'admin'])->group(function () {
+    Route::post('/categories', [CategoryController::class, 'store']);
+    Route::patch('/categories/{category}', [CategoryController::class, 'update']);
+    Route::delete('/categories/{category}', [CategoryController::class, 'destroy']);
+    Route::post('/products', [ProductController::class, 'store']);
+    Route::patch('/products/{product}', [ProductController::class, 'update']);
+    Route::delete('/products/{product}', [ProductController::class, 'destroy']);
+});
