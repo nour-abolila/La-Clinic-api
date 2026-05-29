f73d410c404f5972ff368e46634b70b90eaba17b
nour-abolila <nouraliabolilanaaltya101@gmail.com>
2026-05-29 23:26:19 +0300

    handle app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,8 +1,10 @@
 <?php
 
+use App\Http\Middleware\AdminMiddleware;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -12,7 +14,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+         $middleware->alias(['admin' => AdminMiddleware::class,]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
