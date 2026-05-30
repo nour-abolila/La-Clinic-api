0cdf55c5dd169b5e3b48cdc4fb1694469290d10b
nour-abolila <nouraliabolilanaaltya101@gmail.com>
2026-05-30 18:03:07 +0300

    refactor api.php

--- routes/api.php
+++ routes/api.php
@@ -1,42 +1,61 @@
 <?php
 
-
 use App\Http\Controllers\Auth\AuthController;
 use App\Http\Controllers\Auth\PasswordController;
+use App\Http\Controllers\CartController;
 use App\Http\Controllers\CategoryController;
-use App\Http\Controllers\OrderController;
 use App\Http\Controllers\ProductController;
-use App\Http\Controllers\ReviewController;
-use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 
-Route::prefix('auth')->name('auth.')->group(function () {
-    Route::post('/register', [AuthController::class, 'register']);
-    Route::post('/verify-otp', [AuthController::class, 'verifyOtp']);
-    Route::post('/login', [AuthController::class, 'login']);
-    Route::post('/forget-password', [PasswordController::class, 'forgetPassword']);
-    Route::post('/verify-password', [PasswordController::class, 'verifyPassword']);
-    Route::post('/reset-password', [PasswordController::class, 'resetPassword']);
-});
+// Authentication Routes
 
+Route::prefix('auth')->controller(AuthController::class)->group(function () {
+    Route::post('/register', 'register');
+    Route::post('/verify-otp', 'verifyOtp');
+    Route::post('/login', 'login');
 
-Route::middleware('auth:sanctum')->group(function () {
-    Route::prefix('auth')->name('auth.')->group(function () {
-        Route::post('/logout', [AuthController::class, 'logout']);
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::post('/logout', 'logout');
     });
 });
 
-Route::apiResource('categories', CategoryController::class);
-Route::apiResource('products', ProductController::class);
- 
 
+Route::prefix('auth')->controller(PasswordController::class)->group(function () {
+    Route::post('/forget-password', 'forgetPassword');
+    Route::post('/verify-password', 'verifyPassword');
+    Route::post('/reset-password', 'resetPassword');
+});
+
+
+
+// Public Routes
+
+Route::apiResource('categories', CategoryController::class)
+    ->only(['index', 'show']);
+
+Route::apiResource('products', ProductController::class)
+    ->only(['index', 'show']);
+
+
+
+// Admin Routes
 
 Route::middleware(['auth:sanctum', 'admin'])->group(function () {
-    Route::post('/categories', [CategoryController::class, 'store']);
-    Route::patch('/categories/{category}', [CategoryController::class, 'update']);
-    Route::delete('/categories/{category}', [CategoryController::class, 'destroy']);
-    Route::post('/products', [ProductController::class, 'store']);
-    Route::patch('/products/{product}', [ProductController::class, 'update']);
-    Route::delete('/products/{product}', [ProductController::class, 'destroy']);
+
+    Route::apiResource('categories', CategoryController::class)
+        ->except(['index', 'show']);
+
+    Route::apiResource('products', ProductController::class)
+        ->except(['index', 'show']);
+});
+
+
+
+// Cart Routes
+
+Route::middleware('auth:sanctum')->prefix('cart')->controller(CartController::class)->group(function () {
+    Route::get('/', 'show');
+    Route::post('/add', 'add');
+    Route::delete('/remove/{productId}', 'remove');
 });
