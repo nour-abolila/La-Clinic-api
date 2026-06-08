26fe4d377478c01ee5cb505ffeb4ea15d59e1f7c
nour-abolila <nouraliabolilanaaltya101@gmail.com>
2026-06-08 22:20:47 +0300

    fix some bugs

--- config/auth.php
+++ config/auth.php
@@ -16,7 +16,7 @@ return [
     */
 
     'defaults' => [
-        'guard' => env('AUTH_GUARD', 'web'),
+        'guard' => 'api',
         'passwords' => env('AUTH_PASSWORD_BROKER', 'users'),
     ],
 
@@ -42,6 +42,10 @@ return [
             'driver' => 'session',
             'provider' => 'users',
         ],
+            'api' => [
+                'driver' => 'jwt',
+                'provider' => 'users',
+            ],
     ],
 
     /*
