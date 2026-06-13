5f2b1af576b68ed375f0dfb015ba0e04ada10e78
nour-abolila <nouraliabolilanaaltya101@gmail.com>
2026-06-14 00:24:17 +0300

    refactor: update DoctorProfile model to include session_price, start_time, end_time, and working_days fields

--- app/Models/DoctorProfile.php
+++ app/Models/DoctorProfile.php
@@ -6,11 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class DoctorProfile extends Model
 {
-    protected $fillable = ['user_id', 'specialization', 'years_of_experience', 'bio'];
+    protected $fillable = ['user_id', 'specialization', 'years_of_experience', 'bio' , 'session_price' , 'start_time' , 'end_time' , 'working_days' ];
 
     public function user()
     {
         return $this->belongsTo(User::class);
     }
+
+
+    protected $casts = [
+    'working_days' => 'array',
+];
 
 }
